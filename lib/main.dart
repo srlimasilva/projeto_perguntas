c41183648a5e33322cb1407a6c9c2ca77825a162
Jário <jariolima2020@gmail.com>
2025-02-19 20:32:33 -0300

    Adição de perguntas e modo Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,32 @@ void main(List<String> args) {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
-
-  void responder(){
-    print("Pergunta respondida");
-  }
-
+class PerguntaApp extends StatefulWidget {
 
   const PerguntaApp({super.key});
+
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+
+  var perguntaSelecionada = 0;
+
+
+  void responder(){
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print("Pergunta respondida");
+  }
 
   @override
   Widget build(BuildContext context) {
     final List<String> perguntas = [
       "Qual é a sua cor favorita?",
       "Qual é o seu animal favorito?",
+      "Qual é o animal que faz MU?"
     ];
 
     return MaterialApp(
@@ -28,7 +40,7 @@ class PerguntaApp extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text(perguntas[0]),
+          Text(perguntas[perguntaSelecionada]),
           TextButton(
               child: const Text("Resposta 1"),
               onPressed: () {
@@ -37,13 +49,13 @@ class PerguntaApp extends StatelessWidget {
           TextButton(
             child: const Text("Resposta 2"),
             onPressed: () {
-              null;
+              responder();
             },
           ),
           TextButton(
             child: const Text("Resposta 3"),
             onPressed: () {
-              null;
+              responder();
             },
           ),
         ],
