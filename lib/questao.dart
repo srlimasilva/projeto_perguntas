db2d5614fe7fe43f2101bca8514af7e570fe6330
Jário <jariolima2020@gmail.com>
2025-03-12 17:56:32 -0300

    Correções na mensagem final e ajustando outros requisitos

--- lib/questao.dart
+++ lib/questao.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Questao extends StatelessWidget {
   final String texto;
 
-  Questao(this.texto);
+  const Questao(this.texto, {super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/resposta.dart
+++ lib/resposta.dart
@@ -9,7 +9,7 @@ class Resposta extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
